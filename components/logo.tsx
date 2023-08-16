461dd36b962ec63567d14f4940923afdf7c57491
Aman Ibrahim <amanmibra@gmail.com>
2023-08-16 12:09:58 -0700

    Fix logo theme mismatch

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,14 +4,14 @@ import Image, { type ImageLoaderProps } from "next/image";
 import { useTheme } from "next-themes";
 
 export default function Logo(props: any) {
-  const { theme } = useTheme();
-  const src = theme == "light" ? "/full-light.png" : "/full-dark.png";
+  const { resolvedTheme, setTheme } = useTheme();
+  const src = resolvedTheme == "light" ? "/full-light.png" : "/full-dark.png";
   return <Image {...props} src={src} alt="DeepTruth Logo" />;
 }
 
 function DLogo(props: any) {
-  const { theme } = useTheme();
-  const src = theme == "light" ? "/d-light" : "/d-dark.png";
+  const { resolvedTheme } = useTheme();
+  const src = resolvedTheme == "light" ? "/d-light" : "/d-dark.png";
   return <Image {...props} src={src} alt="DeepTruth Logo" />;
 }
 
