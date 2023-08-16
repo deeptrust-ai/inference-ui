1990c4939a294a4e02c19f66d9affe61396a21eb
Aman Ibrahim <amanmibra@gmail.com>
2023-08-16 15:06:52 -0700

    Fix text

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center text-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <div className="grid grid-flow-row auto-rows-max text-center items-center justify-center">
+        <div className="grid grid-flow-row grid-rows-2 auto-rows-max text-center items-center justify-center">
           <Logo width={456} height={200} />
-          <div className="group text-center text-base justify-between font-mono text-xlg lg:flex">
+          <div className="group text-center justify-between font-mono text-xlg lg:flex text-3xl font-semibold">
             Protect Human Authenticity
           </div>
         </div>
