f2006f3eb2169712c996044975bcf763f3cfde3e
Aman Ibrahim <amanmibra@gmail.com>
2023-08-16 14:56:21 -0700

    Remove light attribute - temporary

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <NavBar />
           {children}
         </ThemeProvider>
