64a1714fa069db1afdb679dbe0a8d2c04e5ff739
Aman Ibrahim <amanmibra@gmail.com>
2023-08-18 08:34:27 -0700

    add health check ping

--- app/demo/model-tester.tsx
+++ app/demo/model-tester.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SVGProps, useState } from "react";
+import { SVGProps, useEffect, useState } from "react";
 
 // shadcn
 import {
@@ -22,12 +22,25 @@ import PredictCards, { type PredictCard } from "./predict-cards";
 
 const MODEL_TYPES = ["ss", "xgb", "cnn"];
 
+// ping server to boot it
+const healthCheck = async () => {
+  return await fetch("/api/", { mode: "no-cors" });
+};
+
 export default function ModelTester() {
   const [modelType, setModelType] = useState<string | null>(null);
   const [file, setFile] = useState<File | null>();
   const [cards, setCards] = useState<PredictCard[]>([]);
   const [loadingMsg, setLoadingMsg] = useState<string | null>(null);
   const [errMsg, setErrMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    try {
+      healthCheck();
+    } catch (err) {
+      console.error("Boot up failed", err);
+    }
+  }, []);
 
   const onPredict = async () => {
     if (!modelType || !file) return;
