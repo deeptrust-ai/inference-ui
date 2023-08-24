54a1264b7261b02a9d4389d60c03000456fa31ef
Aman Ibrahim <amanmibra@gmail.com>
2023-08-24 07:21:09 -0700

    Add modal dev url

--- utils/url.ts
+++ utils/url.ts
@@ -1,6 +1,13 @@
+const localDev = "http://localhost:8000/api/";
+const modalDev =
+  "https://amanmibra--rn-demo-api-modal-serve-dev.modal.run/api/";
+const modalProd = "https://amanmibra--rn-demo-api-modal-serve.modal.run/api/";
+
 const apiURLPrefix =
   process.env.NODE_ENV == "development"
-    ? "http://localhost:8000/api/"
-    : "https://amanmibra--rn-demo-api-modal-serve.modal.run/api/";
+    ? process.env.MODAL_DEV
+      ? modalDev
+      : localDev
+    : modalProd;
 
 export default apiURLPrefix;
