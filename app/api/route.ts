8a8ca5ff0fab54b2ceca1b2e6e8be62760f101f2
Aman Ibrahim <amanmibra@gmail.com>
2023-08-23 21:48:04 -0700

    Update url prefix

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
-import URL from "@/utils/url";
+import apiURLPrefix from "@/utils/url";
 import { NextResponse } from "next/server";
 export async function GET() {
-  const res = await fetch(URL);
+  const res = await fetch(apiURLPrefix);
   const data = await res.json();
   return NextResponse.json(data);
 }
