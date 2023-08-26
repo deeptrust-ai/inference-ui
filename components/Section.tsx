6860636943fc51a36a6ae5698c3d67243c81c1b3
Aman Ibrahim <amanmibra@gmail.com>
2023-08-25 17:33:35 -0700

    edit section text color

--- components/Section.tsx
+++ components/Section.tsx
@@ -14,12 +14,12 @@ const Section = (props: ISectionProps) => (
     }`}
   >
     {(props.title || props.description) && (
-      <div className="mb-12 text-center">
-        {props.title && (
-          <h2 className="text-4xl font-bold text-gray-900">{props.title}</h2>
-        )}
+      <div className="mb-12">
+        {props.title && <h2 className="text-4xl font-bold ">{props.title}</h2>}
         {props.description && (
-          <div className="mt-4 text-xl md:px-20">{props.description}</div>
+          <div className="mt-4 text-xl md:px-20 text-slate-400">
+            {props.description}
+          </div>
         )}
       </div>
     )}
