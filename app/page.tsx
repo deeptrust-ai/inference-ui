a5ffde7c48894f211e2664941e054eddc6aeff12
Aman Ibrahim <amanmibra@gmail.com>
2023-08-25 23:19:55 -0700

    Move Toaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 // import DefaultHome from "./default-home";
-import { Toaster } from "@/components/ui/toaster";
 import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div>
       <Hero />
-      <Toaster />
     </div>
   );
 }
