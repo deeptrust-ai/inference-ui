ea2fdcee626061207df1194ed388cb3b9880663c
Aman Ibrahim <amanmibra@gmail.com>
2023-08-26 15:50:34 -0700

    Add JobCard

--- components/job/Jobs.tsx
+++ components/job/Jobs.tsx
@@ -18,86 +18,97 @@ import { getJobs as getJobsLS, setJob as setJobLS } from "@/utils/localStorage";
 // types
 import { JobOutput, JobOutputs } from "@/types/job";
 import { Button } from "../ui/button";
-import { Loader2 } from "lucide-react";
+import { Loader2, RotateCw } from "lucide-react";
 
 export default function Jobs() {
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const defaultJobs = getJobsLS();
-  const [jobs, setJobs] = useState<JobOutputs>(defaultJobs);
+  const jobs = getJobsLS();
   const jobIds = Object.keys(jobs);
-
-  const onStatusClick = async (jobID: string) => {
-    setLoading(true);
-    const status = await fetch(`/api/job/${jobID}`);
-    const statusData: JobOutput = await status.json();
-
-    setJobLS(jobID, statusData);
-    jobs[jobID] = statusData;
-    // setJobs(jobs)
-    setLoading(false);
-  };
+  console.log(jobIds);
 
   return (
-    <div>
-      {jobIds.map((jobID, index) => {
-        const job: JobOutput = jobs[jobID];
-        const genPercentage = job.gen_percentage;
-        let color = "bg-slate-500";
-        if (genPercentage) {
-          color =
-            genPercentage < 0.5
-              ? "bg-green-600"
-              : genPercentage < 0.8
-              ? "bg-yellow-500"
-              : "bg-red-500";
-        }
-        return (
-          <Card
-            key={index}
-            className={`flex flex-col justify-center m-12 ${color}`}
-          >
-            <CardHeader>
-              <CardTitle>Speech Analysis Job</CardTitle>
-              <CardDescription className="text-slate-200 gap-2 flex flex-row justify-normal">
-                <Badge
-                  className={`${
-                    genPercentage ? "bg-green-200" : "bg-slate-400"
-                  }`}
-                >
-                  {genPercentage ? "Completed" : "Waiting"}{" "}
-                </Badge>
-                {genPercentage == null && (
-                  <p>Click button below to check status of job manually.</p>
-                )}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              {genPercentage ? (
-                <p>
-                  According to deeptruth, this audio has a{" "}
-                  <b>{genPercentage * 100}%</b> of being being generated.
-                </p>
-              ) : (
-                <div>
-                  <p className="mb-2">
-                    It takes a few minutes for a speech analysis job to
-                    complete. Click to check if it has finished.
-                  </p>
-                  <Button onClick={() => onStatusClick(jobID)}>
-                    Check Job Status{" "}
-                    {loading && <Loader2 className="animate-spin" />}
-                  </Button>
-                </div>
-              )}
-            </CardContent>
-            <CardFooter className="gap-2">
-              <Badge variant={"outline"}>DeepTruth Alpha</Badge>
-              {/* <Badge>Model: deeptruth-{modelType}</Badge> */}
-            </CardFooter>
-          </Card>
-        );
-      })}
+    <div className="flex flex-col-reverse">
+      {jobIds.map((jobID, index) => (
+        <JobCard index={index} jobID={jobID} job={jobs[jobID]} />
+      ))}
     </div>
   );
 }
+
+type jobCardType = {
+  index: number;
+  job: JobOutput;
+  jobID: string;
+};
+
+const JobCard = (props: jobCardType) => {
+  let { index, job, jobID } = props;
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onStatusClick = async () => {
+    setLoading(true);
+    const status = await fetch(`/api/job/${jobID}`);
+    job = await status.json();
+
+    setJobLS(jobID, job);
+    setLoading(false);
+  };
+
+  const genPercentage = job.gen_percentage;
+  console.log("job", index);
+  let color = "bg-slate-500";
+  if (genPercentage) {
+    color =
+      genPercentage < 0.5
+        ? "bg-green-600"
+        : genPercentage < 0.8
+        ? "bg-yellow-500"
+        : "bg-red-500";
+  }
+
+  return (
+    <Card key={index} className={`flex flex-col justify-center m-12 ${color}`}>
+      <CardHeader>
+        <CardTitle className="flex flex-row justify-between items-center">
+          <div className="flex gap-2">
+            {" "}
+            <Badge>{index}</Badge>
+            Speech Analysis Job
+          </div>
+          {genPercentage == null && (
+            <RotateCw
+              onClick={onStatusClick}
+              className={`${loading && "animate-spin"}`}
+            />
+          )}
+        </CardTitle>
+        <CardDescription className="text-slate-200 gap-2 flex flex-row justify-normal">
+          <Badge
+            className={`${genPercentage ? "bg-green-200" : "bg-slate-400"}`}
+          >
+            {genPercentage ? "Completed" : "Waiting"}{" "}
+          </Badge>
+          {genPercentage == null && (
+            <p className="flex flex-row items-center gap-2">
+              Click <RotateCw size={14} /> button to check status of job
+              manually.
+            </p>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {genPercentage ? (
+          <p>
+            According to deeptruth, this audio has a{" "}
+            <b>{genPercentage * 100}%</b> of being being generated.
+          </p>
+        ) : (
+          <p>Waiting for job to complete...</p>
+        )}
+      </CardContent>
+      <CardFooter className="gap-2">
+        <Badge variant={"outline"}>DeepTruth Alpha</Badge>
+        {/* <Badge>Model: deeptruth-{modelType}</Badge> */}
+      </CardFooter>
+    </Card>
+  );
+};
