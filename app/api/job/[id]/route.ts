3ee6b05cb26888f446e774f1293096fa51012c35
Aman Ibrahim <amanmibra@gmail.com>
2023-08-25 17:52:12 -0700

    Add status check

--- app/api/job/[id]/route.ts
+++ app/api/job/[id]/route.ts
@@ -2,14 +2,21 @@ import apiURLPrefix from "@/utils/url";
 import { NextRequest, NextResponse } from "next/server";
 
 type paramsType = { id: string };
-export default async function GET(
+export async function GET(
   request: NextRequest,
   { params }: { params: paramsType }
 ) {
+  console.log("here");
   const { id } = params;
   const url = apiURLPrefix + `job/${id}`;
 
   const res = await fetch(url);
+  if (res.status == 202) {
+    return NextResponse.json({
+      message: "Job not finished.",
+      gen_percentage: null,
+    });
+  }
   const data = await res.json();
 
   return NextResponse.json(data);
