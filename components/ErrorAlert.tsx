005cd0a7331abbdd5dd1cc2c3699fc5d4f072896
Aman Ibrahim <amanmibra@gmail.com>
2023-09-02 16:05:30 -0700

    Add ErrorAlert

---
+++ components/ErrorAlert.tsx
@@ -0,0 +1,17 @@
+import { Alert, AlertTitle, AlertDescription } from "./ui/alert";
+import { AlertCircle } from "lucide-react";
+
+const ErrorAlert = ({ msg }: { msg: string }) => {
+  console.log("here");
+  return (
+    <div>
+      <Alert variant="destructive">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>{msg}</AlertDescription>
+      </Alert>
+    </div>
+  );
+};
+
+export default ErrorAlert;
