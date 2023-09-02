caebc48076ecc591d5b1e83daf89f2fe4e75f075
Aman Ibrahim <amanmibra@gmail.com>
2023-09-02 09:46:50 -0700

    Add tabs to website

--- app/app/input.tsx
+++ app/app/input.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { useEffect, useState } from "react";
 
+// shadcn
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { FolderUp, TwitterIcon } from "lucide-react";
+
 import FileUpload from "@/components/job/FileUpload";
 import Jobs from "@/components/job/Jobs";
 import { getJobs } from "@/utils/localStorage";
 import { JobInputProps, JobOutputs } from "@/types/job";
+import { Label } from "@/components/ui/label";
 
 export default function Input() {
   const [jobs, setJobsState] = useState<JobOutputs>({});
+  const [inputType, setInputType] = useState<string>("file");
 
   useEffect(() => {
     setJobsState(getJobs());
@@ -20,7 +27,23 @@ export default function Input() {
 
   return (
     <div className="flex flex-col gap-4">
-      <FileUpload {...props} /> {/* TODO: Add URL input (file vs URL) */}
+      {/* Choose Input Type */}
+      <Label htmlFor="tabs">Choose Input Type</Label>
+      <Tabs id="tabs" defaultValue={inputType} className="w-[400px]">
+        <TabsList className="mb-12">
+          <TabsTrigger value="file" className="flex gap-3">
+            File <FolderUp />
+          </TabsTrigger>
+          <TabsTrigger value="twitter" className="flex gap-3">
+            Twitter <TwitterIcon />
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="file">
+          <FileUpload {...props} /> {/* TODO: Add URL input (file vs URL) */}
+        </TabsContent>
+        <TabsContent value="twitter">Change your password here.</TabsContent>
+      </Tabs>
+      {/* List of Jobs */}
       <Jobs {...props} />
     </div>
   );
