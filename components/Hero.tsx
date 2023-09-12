fbc24cc2a37175ff0710149b850c00425dc1ccb8
Aman Ibrahim <amanmibra@gmail.com>
2023-09-12 15:06:37 -0700

    Update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,31 +8,24 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <div className="text-slate-500 antialiased flex flex-col items-center justify-center text-center">
-      <Logo className="pt-12" width={456} height={200} />
       <Section yPadding="py-12">
         <header className="text-center">
-          <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-white">
-            {"Silencing Deception,\n Protecting Human "}
+          <h1 className="whitespace-pre-line text-4xl font-bold leading-hero text-white">
+            {"Protect Human "}
             <span className="relative whitespace-nowrap text-blue-600">
               <SquigglyLines />
-              <span className="relative">Authenticity</span>
-            </span>{" "}
+              <span className="relative">Authenticity.</span>
+            </span>
+            {"\n Safeguard your content with deepfake detection."}
           </h1>
           <div className="mb-16 mt-4 text-2xl">
-            {"Safeguard your content's integrity with deepfake detection."}
+            {
+              "DeepTrust is building a suite of AI-powered tools to detect deepfake content in your audio, images, and text."
+            }
           </div>
         </header>
         <WaitList />
-        <TryItOut />
       </Section>
     </div>
   );
 }
-
-const TryItOut = () => (
-  <Link href="/app" className="p-12">
-    <Button className="bg-blue-500 text-lg text-bold text-white p-6 mt-12 hover:text-black">
-      Try It Out!
-    </Button>
-  </Link>
-);
