8a37e370ff62816474950ce9d2e9dab1bd59e395
Aman Ibrahim <amanmibra@gmail.com>
2023-11-17 17:54:00 -0800

    Update navbar and add pages

---
+++ app/disinformation/audio/page.tsx
@@ -0,0 +1,9 @@
+import Section from "@/components/Section";
+
+export default function Page() {
+  return (
+    <Section>
+      <h1 className="text-xl">Coming Soon!</h1>
+    </Section>
+  );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,10 @@
 "use client";
+import * as React from "react";
+
 import Link from "next/link";
 import Logo, { DLogo } from "./logo";
 
+// shadcn
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -13,10 +16,39 @@ import {
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 
-// TODO: Add trigger logic for any trigger navbar menu items
-const navBarContent: { title: string; link?: string; trigger?: boolean }[] = [
-  { title: "Speech Detection", link: "/" },
+type ItemType = { title: string; link?: string; desc?: string };
+
+const deepfakeList: ItemType[] = [
+  {
+    title: "Voice Clone Detection",
+    link: "/deepfake/speech",
+    desc: "Detect voice clones in .wav files or twitter videos.",
+  },
+  {
+    title: "Deepfake Image Detection",
+    link: "/deepfake/image",
+    desc: "Detect deepfake images.",
+  },
+  {
+    title: "Deepfake Video Detection",
+    link: "/deepfake/video",
+    desc: "Detect deepfake images.",
+  },
+];
+
+const disinformationList: ItemType[] = [
+  {
+    title: "Video Content Misinformation",
+    link: "/disinformation/video",
+    desc: "Transcribe videos and fact check the content.",
+  },
+  {
+    title: "Audio Content Misinformation",
+    link: "/disinformation/audio",
+    desc: "Transcribe audio and fact check the content.",
+  },
 ];
 
 const genLink = (title: string, link: string) => (
@@ -28,6 +60,16 @@ const genLink = (title: string, link: string) => (
 );
 
 export default function NavBar() {
+  const genList = (list: ItemType[]) => (
+    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+      {list.map((item) => (
+        <ListItem key={item.title} title={item.title} href={item.link}>
+          {item.desc}
+        </ListItem>
+      ))}
+    </ul>
+  );
+
   return (
     <NavigationMenu className="p-6">
       <NavigationMenuList>
@@ -36,12 +78,45 @@ export default function NavBar() {
             <Logo width={100} height={24} />
           </Link>
         </NavigationMenuItem>
-        {navBarContent.map((item) => (
-          <NavigationMenuItem key={item.title}>
-            {item.link && genLink(item.title, item.link)}
-          </NavigationMenuItem>
-        ))}
+        <NavigationMenuItem className="ml-12">
+          <NavigationMenuTrigger>Deepfake Detection</NavigationMenuTrigger>
+          <NavigationMenuContent>{genList(deepfakeList)}</NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Disinformation Detection
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="bg-red">
+            {genList(disinformationList)}
+          </NavigationMenuContent>
+        </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
