b1336313be3d9917c9d78c92f3f9c39957881f37
Aman Ibrahim <amanmibra@gmail.com>
2023-11-20 00:29:11 -0800

    Create "/edge/transcribe/twitter"

---
+++ app/edge/transcribe/twitter/route.ts
@@ -0,0 +1,19 @@
+import apiURLPrefix from "@/utils/url";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+
+  const tweetURL = searchParams.get("url");
+
+  if (tweetURL == null) {
+    console.error("'url' param was not found");
+    return NextResponse.error();
+  }
+
+  const reqURL = apiURLPrefix + "/transcribe/twitter";
+  const response = await fetch(reqURL);
+  const data = await response.json();
+
+  return NextResponse.json(data);
+}
