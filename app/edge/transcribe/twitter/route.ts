13bc16a97aac95226461109ef80a8e582bc21501
Aman Ibrahim <amanmibra@gmail.com>
2023-11-20 11:35:21 -0500

    Added try catch to transcribe route

--- app/edge/transcribe/twitter/route.ts
+++ app/edge/transcribe/twitter/route.ts
@@ -5,15 +5,22 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
   const tweetURL = searchParams.get("url");
+  console.log(`Transcribing (${tweetURL})...`);
 
   if (tweetURL == null) {
     console.error("'url' param was not found");
     return NextResponse.error();
   }
 
-  const reqURL = apiURLPrefix + "/transcribe/twitter";
-  const response = await fetch(reqURL);
-  const data = await response.json();
+  try {
+    const reqURL = new URL(apiURLPrefix + "transcribe/twitter");
+    reqURL.searchParams.append("url", tweetURL);
+    const response = await fetch(reqURL);
+    const data = await response.json();
 
-  return NextResponse.json(data);
+    return NextResponse.json(data);
+  } catch (err: any) {
+    console.error(`Transcription Failed: ${err.message}`);
+    return NextResponse.error();
+  }
 }
