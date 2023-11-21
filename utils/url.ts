f3f811c5c7cedc546063b27e47e2aa66ba496f61
Aman Ibrahim <amanmibra@gmail.com>
2023-11-21 00:19:09 -0500

    undo url changes

--- utils/url.ts
+++ utils/url.ts
@@ -3,10 +3,10 @@ const modalDev = "https://amanmibra--rawnet-api-api-dev.modal.run/api/";
 const modalProd = "https://amanmibra--rawnet-api-api.modal.run/api/";
 
 const apiURLPrefix =
-  process.env.API_MODE == "prod"
-    ? modalProd
-    : process.env.MODAL_DEV
-    ? modalDev
-    : localDev;
+  process.env.NODE_ENV == "development"
+    ? process.env.MODAL_DEV
+      ? modalDev
+      : localDev
+    : modalProd;
 
 export default apiURLPrefix;
