7f790ae84bdf4cc31bb3d2156fd1ed2b5fdeac72
Aman Ibrahim <amanmibra@gmail.com>
2023-12-08 08:33:57 -0500

    Add conditional wrapper for links

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
 
 // components
 import Section from "@/components/Section";
+import Link from "next/link";
 
 type ItemType = { title: string; link?: string; desc?: string };
 
@@ -45,22 +46,49 @@ const verityList: ItemType[] = [
   },
 ];
 
+const ConditionalWrapper = ({ condition, wrapper, children }: any) =>
+  condition ? wrapper(children) : children;
+
 export default function Page() {
   return (
-    <Section>
-      <h2 className="">Choose one of the demo options</h2>
+    <div className="flex m-12 flex-col gap-2">
+      <h2 className="text-xl">Choose one of the demo options</h2>
       {/* Deepfake Cards */}
-      <h3>Deepfake Detection Tools</h3>
-      <div className="grid grid-flow-col">
-        {deepfakeList.map((item) => (
-          <Card key={item.title}>
-            <CardTitle>{item.title}</CardTitle>
-            <CardContent>
-              <p>{item.desc}</p>
-            </CardContent>
-          </Card>
-        ))}
+      <h3 className="text-bold">Deepfake Detection Tools</h3>
+      <div className="grid grid-flow-col grid-cols-3">
+        {deepfakeList.map((item) => {
+          let className = "m-6 text-black py-3 px-2";
+
+          if (item.link) {
+            className += " bg-slate-200 hover:bg-slate-500 hover:text-white";
+          } else {
+            className += " bg-slate-700";
+          }
+
+          return (
+            <ConditionalWrapper
+              condition={item.link}
+              wrapper={(children: any) => (
+                <Link href={item.link || ""}>{children}</Link>
+              )}
+            >
+              <Card className={className} key={item.title}>
+                <CardHeader>
+                  <CardTitle>{item.title}</CardTitle>
+                  {!item.link && (
+                    <CardDescription className="text-black">
+                      Coming Soon
+                    </CardDescription>
+                  )}
+                </CardHeader>
+                <CardContent>
+                  <p>{item.desc}</p>
+                </CardContent>
+              </Card>
+            </ConditionalWrapper>
+          );
+        })}
       </div>
-    </Section>
+    </div>
   );
 }
