85835ffafa5f18bd7eb813b69c41d5b65bc12c37
Aman Ibrahim <amanmibra@gmail.com>
2023-12-08 14:07:53 -0500

    Remove deps

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,20 +4,6 @@ import * as React from "react";
 import Link from "next/link";
 import Logo, { DLogo } from "./logo";
 
-// shadcn
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
-
 export default function NavBar() {
   return (
     <div className="p-6">
