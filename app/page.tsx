010005964ea412f5e4b31c3dbba3682396e1df9c
Aman Ibrahim <amanmibra@gmail.com>
2023-12-08 00:23:24 -0500

    Add basic cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,66 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 // components
 import Section from "@/components/Section";
 
+type ItemType = { title: string; link?: string; desc?: string };
+
+const deepfakeList: ItemType[] = [
+  {
+    title: "Voice Clone Detection",
+    link: "/deepfake/speech",
+    desc: "Detect voice clones in .wav files or twitter videos.",
+  },
+  {
+    title: "Deepfake Image Detection",
+    desc: "Detect deepfake images.",
+  },
+  {
+    title: "Deepfake Video Detection",
+    desc: "Detect deepfake images.",
+  },
+];
+
+const verityList: ItemType[] = [
+  {
+    title: "Twitter Content Misinformation",
+    link: "/verity/twitter",
+    desc: "Transcribe twitter videos and fact check the content.",
+  },
+  {
+    title: "Text Content Misinformation",
+    link: "/verity/text",
+    desc: "Fact check text-based content (articles, transciptions, etc).",
+  },
+  {
+    title: "Audio Content Misinformation",
+    desc: "Transcribe audio and fact check the content.",
+  },
+];
+
 export default function Page() {
   return (
-    <Section yPadding="py-12">
-      <div className="mb-4">Choose one of the demo options above</div>
+    <Section>
+      <h2 className="">Choose one of the demo options</h2>
+      {/* Deepfake Cards */}
+      <h3>Deepfake Detection Tools</h3>
+      <div className="grid grid-flow-col">
+        {deepfakeList.map((item) => (
+          <Card key={item.title}>
+            <CardTitle>{item.title}</CardTitle>
+            <CardContent>
+              <p>{item.desc}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </Section>
   );
 }
