417c4365c55826d09fd3ffc234131e818a8738b0
Aman Ibrahim <amanmibra@gmail.com>
2023-12-23 01:03:33 -0500

    Add logout button to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,14 +2,25 @@
 import * as React from "react";
 
 import Link from "next/link";
+
+import { useAuthInfo, useLogoutFunction } from "@propelauth/react";
+
 import Logo, { DLogo } from "./logo";
+import { Button } from "./ui/button";
+
+const LogoutButton = () => {
+  const logoutFn = useLogoutFunction(); // (1)
+  return <Button onClick={() => logoutFn(true) /*(2)*/}>Logout</Button>;
+};
 
 export default function NavBar() {
+  const authInfo = useAuthInfo();
   return (
     <div className="p-6">
       <Link href={"/"}>
         <Logo width={100} height={24} />
       </Link>
+      {authInfo.isLoggedIn && <LogoutButton />}
     </div>
   );
 }
