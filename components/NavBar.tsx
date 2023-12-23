1caebbd1b8bc9b442bc1103c9a3f937c5c081548
Aman Ibrahim <amanmibra@gmail.com>
2023-12-23 07:32:59 -0500

    Switch to profile dropdown

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,26 +1,61 @@
 "use client";
-import * as React from "react";
-
 import Link from "next/link";
 
-import { useAuthInfo, useLogoutFunction } from "@propelauth/react";
+// auth
+import {
+  useAuthInfo,
+  useLogoutFunction,
+  useRedirectFunctions,
+} from "@propelauth/react";
 
-import Logo, { DLogo } from "./logo";
+// ui
 import { Button } from "./ui/button";
+import {
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenu,
+} from "@/components/ui/dropdown-menu";
 
-const LogoutButton = () => {
-  const logoutFn = useLogoutFunction(); // (1)
-  return <Button onClick={() => logoutFn(true) /*(2)*/}>Logout</Button>;
+import Logo from "./logo";
+import { LogOut, User, UserCog } from "lucide-react";
+
+const ProfileDropDown = () => {
+  const { redirectToAccountPage } = useRedirectFunctions();
+  const logoutFn = useLogoutFunction();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="rounded-full" variant="outline">
+          <User />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-2">
+        <DropdownMenuItem onClick={redirectToAccountPage}>
+          <div className="flex items-center space-x-2">
+            <UserCog />
+            <span>Account</span>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => logoutFn(true)}>
+          <div className="flex items-center space-x-2">
+            <LogOut />
+            <span>Logout</span>
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default function NavBar() {
   const authInfo = useAuthInfo();
   return (
-    <div className="flex p-6 justify-between">
+    <div className="flex m-6 justify-between">
       <Link href={"/"}>
         <Logo width={100} height={24} />
       </Link>
-      {authInfo.isLoggedIn && <LogoutButton />}
+      {authInfo.isLoggedIn && <ProfileDropDown />}
     </div>
   );
 }
