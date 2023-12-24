76dae89759c4a9d077da2c7fe9dac6adec0f361a
Aman Ibrahim <amanmibra@gmail.com>
2023-12-24 11:03:34 -0500

    Update api url

--- utils/url.ts
+++ utils/url.ts
@@ -1,6 +1,6 @@
 const localDev = "http://localhost:8000/api/";
 const modalDev = "https://amanmibra--rawnet-api-api-dev.modal.run/api/";
-const modalProd = "https://amanmibra--rawnet-api-api.modal.run/api/";
+const modalProd = "https://api.deeptrustai.com/api/";
 
 const apiURLPrefix =
   process.env.NODE_ENV == "development"
